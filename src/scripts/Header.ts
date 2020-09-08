2c08d629aaf172acca615ac924552bc19e5a20cf
Daniil <dan28012000@gmail.com>
2020-09-08 13:12:17 +0500

    заверешиене работы над шапкой сайта

--- src/scripts/Header.ts
+++ src/scripts/Header.ts
@@ -3,14 +3,23 @@ import { findElement, findAllElements } from './utils';
 export default class Header {
   private $header: HTMLElement;
   private $navLink: NodeListOf<HTMLElement>;
+  private $mobileNavaLinl: NodeListOf<HTMLElement>;
+  private $hamburger: HTMLElement;
+  private $mobileMenu: HTMLElement;
   private headerHeight: number;
-  private greetingHeight: number;
 
   constructor() {
     this.$header = findElement('.header');
     this.$navLink = findAllElements('.header__item a');
-    this.headerHeight = +getComputedStyle(findElement(':root')).getPropertyValue('--headerHeight').split('px')[0];
-    this.greetingHeight = findElement('.greeting').offsetHeight;
+    this.$mobileNavaLinl = findAllElements('.header__menu a');
+    this.$hamburger = findElement('.header__humburger');
+    this.$mobileMenu = findElement('.header__menu');
+    if (window.screen.width > 575) {
+      this.headerHeight = +getComputedStyle(findElement(':root')).getPropertyValue('--headerHeight').split('px')[0];
+    } else {
+      this.headerHeight = +getComputedStyle(findElement(':root')).getPropertyValue('--headerHeightMobile').split('px')[0];
+    }
+
     this.addListeners();
     this.initNavigation();
   }
@@ -44,24 +53,33 @@ export default class Header {
   }
 
   private addListeners(): void {
-    this.$navLink.forEach((link) => {
+    [...this.$navLink, ...this.$mobileNavaLinl].forEach((link) => {
       link.addEventListener('click', (e) => {
         const element = e.target as HTMLElement,
-          classToScroll = element.dataset['to'];
+          classToScroll = element.dataset['to'],
+          parent = element.parentElement;
 
         this.clearActive();
         element.classList.add('header__item_active');
+        if (parent.classList.contains('header__menu')) {
+          this.$hamburger.classList.toggle('header__humburger_active');
+          this.$mobileMenu.classList.toggle('header__menu_active');
+        }
         this.scrollToElem(classToScroll);
       });
+    });
+    this.$hamburger.addEventListener('click', () => {
+      this.$hamburger.classList.toggle('header__humburger_active');
+      this.$mobileMenu.classList.toggle('header__menu_active');
     });
   }
 
   private clearActive(): void {
-    this.$navLink.forEach((link) => link.classList.remove('header__item_active'));
+    [...this.$navLink, ...this.$mobileNavaLinl].forEach((link) => link.classList.remove('header__item_active'));
   }
 
   private addActive(section: string): void {
-    this.$navLink.forEach((link) => {
+    [...this.$navLink, ...this.$mobileNavaLinl].forEach((link) => {
       if (link.dataset['to'] === section) {
         link.classList.add('header__item_active');
       }
